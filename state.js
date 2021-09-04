63e559e5c761b5466e3a8d4a16a522dbc8945745
Bera Koc <berakoc97@gmail.com>
2021-09-04 14:35:16 +0300

    Seperated Utils and formatted code

--- state.js
+++ state.js
@@ -1,35 +1,31 @@
+const Utils = {
+  $: (query, context = document) => context.querySelectorAll(query),
+  is: Object.is,
+  type: (v) => typeof v,
+  stringToObject: (value, options = { isArray: false, isString: false }) =>
+    options.isString
+      ? value.replace(/'/g, '')
+      : JSON.parse(options.isArray ? value.replace(/'/g, '"') : value),
+};
+
 (() => {
-  const $ = (query, context = document) => context.querySelectorAll(query);
-  const is = Object.is;
-  const type = v => typeof(v);
-
-  const getStateHandlers = () => $('[bean]');
-
-  const getGlobalListener = (listenerName) => listeners[listenerName];
-
   const StateHandler = (() => {
     let state = {};
     return {
       getState: () => state,
       setState: (key, value) => {
-        if(is(state[key], value)) return;
-        state = ({
+        if (is(state[key], value)) return;
+        state = {
           ...state,
-          [key]: value
-        });
-      }
-    }
+          [key]: value,
+        };
+      },
+    };
   })();
 
   const StateHandlerTuple = [StateHandler.getState, StateHandler.setState];
-
-  const stringToObject = (value, options = { isArray: false, isString: false }) =>
-    options.isString
-      ? value.replace(/'/g, '')
-      : JSON.parse(options.isArray 
-        ? value.replace(/'/g, '"') 
-        : value
-  );
+  const getStateHandlers = () => $('[bean]');
+  const getGlobalListener = (listenerName) => listeners[listenerName];
 
   const createStateContainers = (stringTokens, stateId, value) => {
     const HTMLFragments = [];
@@ -43,44 +39,50 @@
       }
     });
     return HTMLFragments;
-  }
+  };
 
   const initialRender = (bean, stateId) => {
     const rawInitialValue = bean.getAttribute('init');
     const value = stringToObject(rawInitialValue, {
-      isString: Boolean(~rawInitialValue.indexOf('\''))
+      isString: Boolean(~rawInitialValue.indexOf("'")),
     });
     StateHandler.setState(stateId, value);
-    $(`[${stateId}]`, bean).forEach(
-      (node) => {
-        node.hasAttribute(stateId) && node.removeAttribute(stateId);
-        const stringTokens = node.innerHTML.split(`$state`);
-        const HTMLFragments = createStateContainers(stringTokens, stateId, value);
-        const enhancedHTML = HTMLFragments.map(HTMLFragment => is(type(HTMLFragment), 'string') ? document.createTextNode(HTMLFragment) : HTMLFragment);
-        node.innerHTML = null;
-        node.append(...enhancedHTML);
-      }
-    );
+    $(`[${stateId}]`, bean).forEach((node) => {
+      node.hasAttribute(stateId) && node.removeAttribute(stateId);
+      const stringTokens = node.innerHTML.split(`$state`);
+      const HTMLFragments = createStateContainers(stringTokens, stateId, value);
+      const enhancedHTML = HTMLFragments.map((HTMLFragment) =>
+        is(type(HTMLFragment), 'string')
+          ? document.createTextNode(HTMLFragment)
+          : HTMLFragment
+      );
+      node.innerHTML = null;
+      node.append(...enhancedHTML);
+    });
   };
 
   const render = (bean, stateId) => {
     const value = StateHandler.getState()[stateId];
-    $(`[${stateId}]`, bean).forEach(
-      (node) => node.innerText = value
-    );
+    $(`[${stateId}]`, bean).forEach((node) => (node.innerText = value));
   };
 
   const injectActions = (bean, stateId) => {
     const actionAttributeName = `action-${stateId}`;
     const actionElements = $(`[${actionAttributeName}]`, bean);
-    actionElements.forEach(actionElement => {
-      const [listenerName, type] = stringToObject(actionElement.getAttribute(actionAttributeName), {
-        isArray: true
-      });
+    actionElements.forEach((actionElement) => {
+      const [listenerName, type] = stringToObject(
+        actionElement.getAttribute(actionAttributeName),
+        {
+          isArray: true,
+        }
+      );
       actionElement.addEventListener(type, () => {
         let _actionElement = actionElement;
         actionElement instanceof HTMLInputElement
-          ? getGlobalListener(listenerName)(() => _actionElement['value'], ...StateHandlerTuple)
+          ? getGlobalListener(listenerName)(
+              () => _actionElement['value'],
+              ...StateHandlerTuple
+            )
           : getGlobalListener(listenerName)(...StateHandlerTuple);
         render(bean, stateId);
       });

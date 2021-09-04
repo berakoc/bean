2d97fc3193e0dd674e537c7e03997e0c1d15ee98
Bera Koc <berakoc97@gmail.com>
2021-09-04 20:00:29 +0300

    Implemented build pipe

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,1 +1,11 @@
+echo "Build is about to start"
+echo "Clearing dist folder..."
+rm -rf dist
+echo "Dist folder is removed"
+echo "Transpiling the source code..."
+npx babel src/index.js -o dist/index.js
+echo "Source code is transpiled"
+echo "Minifying the transpiled code"
+uglifyjs dist/index.js -o dist/index.min.js --compress --mangle --source-map url=index.min.js.map
+echo "Minification is over"
 echo "Build is ready"

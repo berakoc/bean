72c2a2ca6657924998bc3881b9eb952866201a75
Bera Koc <berakoc97@gmail.com>
2021-09-04 20:09:39 +0300

    Added colors for build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,14 +1,17 @@
+LIGHT_GREEN='\033[1;32m'
+LIGHT_YELLOW='\033[1;33m'
 LIGHT_PURPLE='\033[1;35m'
+LIGHT_BLUE='\033[1;34m'
 NC="\033[0m"
 
-printf "${LIGHT_PURPLE}Build is about to start"
-echo "Clearing dist folder..."
+printf "${LIGHT_PURPLE}Build is about to start\n"
+printf "${LIGHT_YELLOW}Clearing dist folder...\n"
 rm -rf dist
-echo "Dist folder is removed"
-echo "Transpiling the source code..."
+printf "${LIGHT_GREEN}Dist folder is removed\n"
+printf "${LIGHT_YELLOW}Transpiling the source code...\n"
 npx babel src/index.js -o dist/index.js
-echo "Source code is transpiled"
-echo "Minifying the transpiled code"
+printf "${LIGHT_GREEN}Source code is transpiled\n"
+printf "${LIGHT_YELLOW}Minifying the transpiled code\n"
 uglifyjs dist/index.js -o dist/index.min.js --compress --mangle --source-map url=index.min.js.map
-echo "Minification is over"
-echo "Build is ready"
+printf "${LIGHT_GREEN}Minification is completed\n"
+printf "${LIGHT_BLUE}Build is ready\n"
